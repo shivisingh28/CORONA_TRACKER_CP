851a9b7513b07e4ba0981996c100f4a177bd19c7
shivisingh28 <shivi2819997@gmail.com>
2020-09-29 01:38:21 +0530

    Line Chart finished

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -49,7 +49,7 @@ const options = {
 	},
 };
 
-function LineGraph() {
+function LineGraph({casesType}) {
 	const [data, setData] = useState({});
 
 	const buildChartData = (data, casesType = "cases") => {
@@ -69,16 +69,22 @@ function LineGraph() {
 		return chartData;
 	};
 
-	useEffect(() => {
-		fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
-			.then((response) => response.json())
-			.then((data) => {
-				console.log(data);
-				const chartData = buildChartData(data);
-				setData(chartData);
-			});
-	}, []);
-
+    useEffect(() => {
+        const fetchData = async () => {
+          await fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
+            .then((response) => {
+              return response.json();
+            })
+            .then((data) => {
+              let chartData = buildChartData(data, casesType);
+              setData(chartData);
+              console.log(chartData);
+              // buildChart(chartData);
+            });
+        };
+    
+        fetchData();
+      }, [casesType]);
 	return (
 		<div>
 			{data?.length > 0 && (
